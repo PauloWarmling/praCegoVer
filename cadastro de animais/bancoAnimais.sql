1f11d65e11125c392736b1c90b352e9306a48523
root <root@localhost.localdomain>
2019-10-10 15:31:03 -0300

    Primeiro upload de arquivos

---
+++ cadastro de animais/bancoAnimais.sql
@@ -0,0 +1,17 @@
+create database engenharia;
+use engenharia;
+
+create table tipoAnimal(
+codigo int auto_increment primary key,
+tipo varchar(45)
+);
+
+create table animais(
+codigo int auto_increment primary key,
+nome varchar(45),
+linkImg varchar(45),
+linkVid varchar(45),
+tipoCodigo int,
+foreign key (tipoCodigo) references tipoAnimal(codigo)
+);
+
---
+++ cadastro de animais/cad_animais.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<?php
+require_once 'banco.php';
+$id = isset($_GET['id'])?$_GET['id']:0;
+$banco = new banco;
+$valores = $banco->select("SELECT * FROM animais where codigo = $id");
+?>
+<head>
+	<title></title>
+</head>
+<body>
+	<a href="index.php">Listagem</a>
+	<form action="acao.php" method="post">
+<?php
+	if(isset($valores[0][0])){
+		echo "<input type='hidden' value='{$valores[0][0]}' name='id'>";
+	}
+?>		
+nome<input type="text" name="nome" <?php if(isset($valores[0][1])){echo "value='{$valores[0][1]}'}";} ?>><br>
+linkImg<input type="text" name="linkImg" <?php if(isset($valores[0][2])){echo "value='{$valores[0][2]}'}";} ?>><br>
+linkVid<input type="text" name="linkVid" <?php if(isset($valores[0][3])){echo "value='{$valores[0][3]}'}";} ?>><br>
+tipoCodigo<input type="text" name="tipoCodigo" <?php if(isset($valores[0][4])){echo "value='{$valores[0][4]}'}";} ?>><br>
+
+<input type="submit" name="acao" value="inserirA">
+</form>
+</body>
+</html>
