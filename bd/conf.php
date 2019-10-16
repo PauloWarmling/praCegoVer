1fb086941ba7edeadc879f47a845eed5786a2404
WrenchEscobar <ju_pn2010@hotmail.com>
2019-10-16 16:31:19 -0300

    Padronização de valores para conexão com o banco de dados

---
+++ bd/conf.php
@@ -0,0 +1,8 @@
+<?php
+
+  $host = "localhost"; // Mudar para servidor da Marcela
+  $database = "engenharia";
+  $usuario = "escola"; // Mudar para usuário da Marcela
+  $senha = "escola"; // Mudar para senha da Marcela
+
+?>
