af7bd98462736077aad545693b23f87d6cb80b67
WrenchEscobar <ju_pn2010@hotmail.com>
2019-10-17 16:19:02 -0300

    Não lembro oq foi feito

--- gestos/bancoComidas.sql
+++ gestos/bancoComidas.sql
@@ -1,12 +1,12 @@
 create database a;
 use engenharia;
 
-create table tipoComida(
+create table tipoGestos(
 codigo int auto_increment primary key,
 tipo varchar(45)
 );
 
-create table comida(
+create table gestos(
 codigo int auto_increment primary key,
 nome varchar(45),
 linkImg varchar(45),
@@ -14,7 +14,3 @@ linkVid varchar(45),
 tipoCodigo int,
 foreign key (tipoCodigo) references tipoComida(codigo)
 );
-
-SELECT * FROM comida;
-update animais set linkImg = 'img/rato.jpg' where codigo = 10;
-
